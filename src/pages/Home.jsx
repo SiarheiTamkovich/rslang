ef55e272e9b3e6114c9b70473efe99f17bab790b
SiarheiTamkovich <mpg2000@mail.ru>
2022-02-18 13:46:04 +0300

    feat: add picture floure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 //import { AddNotes } from "../components/AddNotes/AddNotes";
 import "./Home.scss"
+import floure from "../img/floure.png"
 
 export const Home = () => {
 
@@ -9,6 +10,9 @@ export const Home = () => {
         <h1>RS Lang</h1>
         <h2>ИЗУЧАЕМ ЯЗЫК ИГРАЯ</h2>
         <h3>Простой способ освоения</h3>
+      </div>
+      <div className="home-pict">
+        <img src={floure} alt="floure"/>
       </div>
       {/* <div className="todo-notes">
         <h1 style={{color: "#fff"}}>Task "RS Lang" deadline: 21 feb. 2022</h1>
