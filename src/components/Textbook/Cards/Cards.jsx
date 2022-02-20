e49540fcc716cb24ee7c4e623866dbb129e8dac0
SiarheiTamkovich <mpg2000@mail.ru>
2022-02-21 00:39:18 +0300

    fix: edit catd wods about text

--- src/components/Textbook/Cards/Cards.jsx
+++ src/components/Textbook/Cards/Cards.jsx
@@ -15,12 +15,12 @@ export const Cards = (page) => {
             <div className="card-wrapper">
               <h3 className="eng-word">{elem.word}</h3>
               <audio src={`https://react-learnwords-example.herokuapp.com/${elem.audio}`} controls></audio>
-              <h5 className="word">Перевод: {elem.wordTranslate}</h5>
-              <h5 className="word">Транскрипция: {elem.transcription}</h5>
-              <p className="word-about">{elem.textExample}</p>
+              <h5 className="word">Перевод: <strong>{elem.wordTranslate}</strong></h5>
+              <h5 className="word">Транскрипция: <strong>{elem.transcription}</strong></h5>
+              <p className="word-about">{elem.textExample.replace('<b>', ' ').replace('</b>', ' ')}</p>
               <audio src={`https://react-learnwords-example.herokuapp.com/${elem.audioExample}`} controls></audio>
               <p className="word-about">{elem.textExampleTranslate}</p>
-              <p className="word-about">{elem.textMeaning}</p>
+              <p className="word-about">{elem.textMeaning.replace('<i>', ' ').replace('</i>', ' ')}</p>
               <audio src={`https://react-learnwords-example.herokuapp.com/${elem.audioMeaning}`} controls></audio>
               <p className="word-about">{elem.textMeaningTranslate}</p>
             </div>
