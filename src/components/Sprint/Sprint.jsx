2a4afc47639da2b56c0a480ca5560f04e36a520e
SiarheiTamkovich <mpg2000@mail.ru>
2022-02-20 21:23:57 +0300

    fix: edit color sound pict

--- src/components/Sprint/Sprint.jsx
+++ src/components/Sprint/Sprint.jsx
@@ -4,27 +4,24 @@ import "./Sprint.scss";
 export const Sprint = () => {
 
   return (
-    <div className="sprint-container">
+    <div className="wrapper">
+      <div className="reset hidden"></div>
       <div className="topSection">
         <div className="timer"></div>
         <div className="score"></div>
       </div>
       <div className="question">
-        <div className="correctCount">
-          <div className="count"></div>
-          <div className="count"></div>
-          <div className="count"></div>
-        </div>
         <div className="correctSound"></div>
         <div className="questionNotT"></div>
         <div className="questionT"></div>
       </div>
-      <div className="result"></div>
-      <div className="answer-block">
+      <div className="result hidden"></div>
+      <div className="answer-block hidden">
         <div className="unCorrect">Неверно!</div>
         <div className="correct">Верно!</div>
       </div>
-      <div className="button">Start!</div>
-    </div>
+      <div className="button-sp">Start!</div>
+  </div>
   )
 }
+
